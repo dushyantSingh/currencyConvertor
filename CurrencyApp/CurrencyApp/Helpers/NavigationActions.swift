dca4ed02f5be1748ae3f863c4d71420b80d49331
Dushyant Singh <dushyantsingh1097@gmail.com>
2020-05-31 17:45:17 +0800

    Create Navigation action
    Added navigation actions for main view model coordinator

--- CurrencyApp/CurrencyApp/Helpers/NavigationActions.swift
+++ CurrencyApp/CurrencyApp/Helpers/NavigationActions.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+enum NavigationAction {
+    case push(viewModel: Any, animated: Bool)
+    case present(viewModel: Any, animated:Bool)
+    case pop(animated: Bool)
+    case dismiss(animated: Bool)
+}
+
+extension NavigationAction: Equatable {
+    static func == (lhs: NavigationAction, rhs: NavigationAction) -> Bool {
+        switch (lhs, rhs) {
+        case (.push, .push):
+            return true
+        case (.present, .present):
+            return true
+        case (.pop, .pop):
+            return true
+            case (.dismiss, .dismiss):
+            return true
+        default: return false
+        }
+    }
+}
